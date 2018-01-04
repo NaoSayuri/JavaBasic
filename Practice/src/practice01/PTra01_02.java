0745240b6983ee53ba5b5b24e9fb29a210f0c5da
DESKTOP-38GIBV3\LIKEIT_STUDENT <naosayuri@gmail.com>
2018-01-04 14:09:59 +0900

    １－２

--- Practice/src/practice01/PTra01_02.java
+++ Practice/src/practice01/PTra01_02.java
@@ -10,11 +10,13 @@ public class PTra01_02 {
 	public static void main(String[] args) {
 
 		// ★ 12～14行目をコメントにしてください。
-		ここをコメントにしてください
-		ここをコメントにしてください
-		ここをコメントにしてください
+		// ここをコメントにしてください。
+		//ここをコメントにしてください。
+		//コメントにしました。
 
 		//  ★「Hello, world」と出力（コマンドプロンプトに表示）してください
+		System.out.println("Hello");
+		System.out.println("world");
 
 
 	}
